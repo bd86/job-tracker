4653d359c94db162bb1fb071a47885dac0583d04
Bryan <bryan.davila@charismamedia.com>
2015-07-20 17:34:46 -0400

    added methods

--- js/mainvue.js
+++ js/mainvue.js
@@ -1,24 +1,47 @@
 var demo = new Vue({ 
-    el: '#content',
-    data: {
-        title: 'todos',
-        todos: [
-            {
-                done: false,
-                content: 'Learn JavaScript'
-            },
-            {
-                done: true,
-                content: 'Learn Vue.js'
-            },
-            {
-                done:false,
-                content: 'Move'
-            },
-            {
-                done: true,
-                content: 'still moving'
-            }
-        ]
+  el: '#content',
+  data: {
+    title: 'todos',
+    todos: [
+      {
+        done: false,
+        test: 'SOmething',
+        content: 'Learn JavaScript'
+      },
+      {
+        done: true,
+        test: 'SOmething',
+        content: 'Learn Vue.js'
+      },
+      {
+        done:false,
+        test: 'SOmething',
+        content: 'Move'
+      },
+      {
+        done: true,
+        test: 'SOmething',
+        content: 'still moving'
+      }
+    ],
+    newTask: ''
+  },
+
+  methods: {
+    addTask: function(e) {
+      e.preventDefault();
+      this.todos.push({
+        done: false,
+        test: 'Mayber time',
+        content: this.newTask
+      });
+
+      this.newTask = '';
+    },
+
+    removeTask: function(todo) {
+      console.log(todo);
+      this.todos.$remove(todo);
     }
+  }
 });
