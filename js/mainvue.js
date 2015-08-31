7797dad73b1ff8ff7eb59c0bc505dccdc55d54d5
bd86 <bd86@users.noreply.github.com>
2015-08-30 23:01:04 -0400

    edited todo

--- js/mainvue.js
+++ js/mainvue.js
@@ -17,7 +17,7 @@ var main = new Vue({
       {
         done: false,
         test: 'Something',
-        content: 'kind of Still Fix car'
+        content: 'still kind of Still Fix car'
       },
       {
         done: false,
