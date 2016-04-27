d5a8abdb6b77f7ffd5abea8267bdad486074b916
bd86 <bd86@users.noreply.github.com>
2016-04-26 22:41:04 -0400

    edited todo

--- js/mainvue.js
+++ js/mainvue.js
@@ -62,7 +62,7 @@ var main = new Vue({
       {
         done: false,
         test: 'Something',
-        content: 'still Have worked on this ahhhh?!?!wsf?5?!d?fs!fdseffgs?s?xzascxd!!d3?ad??!??!??!!!'
+        content: 'still Have worked on this ahhhh?!?!wsf?5?!d?fs!fdseffgs?s?xzscxd!!d3?ad??!??!??!!!'
       }
       
     ],
